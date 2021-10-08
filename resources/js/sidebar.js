d9ff914d42465c6ec8cb6b24ac38c91d08ef4161
Arthur van der Leest <avdleest@gmail.com>
2021-10-09 00:05:24 +0200

    Restyled sidebar

--- resources/js/sidebar.js
+++ resources/js/sidebar.js
@@ -1,13 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function switchNav() {
-  const size = '250px'
+  const size = '200px'
   const currentSize = document.getElementById('sidebar').style.width
 
   if (currentSize === size) {
     document.getElementById('sidebar').style.width = '0'
-    document.getElementById('main').style.marginLeft = '0'
+    document.getElementById('sidebar').style.backgroundColor = 'rgba(255, 255, 255, 0)'
+    document.getElementById('sidebar').style.paddingRight = '0'
   } else {
     document.getElementById('sidebar').style.width = size
-    document.getElementById('main').style.marginLeft = size
+    document.getElementById('sidebar').style.backgroundColor = 'rgba(255, 255, 255, 0.95)'
+    document.getElementById('sidebar').style.paddingRight = '30px'
   }
 }
