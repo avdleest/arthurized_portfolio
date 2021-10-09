f77eeb4d5167783e8e3c2f3cc3fe9e010ca81fae
Arthur van der Leest <avdleest@gmail.com>
2021-10-09 10:44:26 +0200

    Fixed opening and closing of nav

--- resources/js/sidebar.js
+++ resources/js/sidebar.js
@@ -1,32 +1,26 @@
-function clickListener() {
-  document.onclick = function (event) {
-    // Compensate for IE<9's non-standard event model
-    //
-    if (event === undefined) event = window.event
-    alert('clicklistener fired')
-    document.removeEventListener('click', clickListener, false)
-    const target = 'target' in event ? event.target : event.srcElement
-    if (!['0px', ''].includes(document.getElementById('sidebar').style.width)) {
-      console.log(document.getElementById('sidebar').style.width)
-      switchNav()
-      document.removeEventListener('click', clickListener, false)
-    }
-  }
-}
+let sidebarVisible = false
 
 function switchNav() {
   const size = '200px'
   const currentSize = document.getElementById('sidebar').style.width
-  document.removeEventListener('click', clickListener, false)
 
   if (currentSize === size) {
     document.getElementById('sidebar').style.width = '0px'
     document.getElementById('sidebar').style.backgroundColor = 'rgba(255, 255, 255, 0)'
     document.getElementById('sidebar').style.paddingRight = '0'
-  } else {
+    sidebarVisible = false
+  } else if (sidebarVisible === false) {
     document.getElementById('sidebar').style.width = size
     document.getElementById('sidebar').style.backgroundColor = 'rgba(255, 255, 255, 0.95)'
     document.getElementById('sidebar').style.paddingRight = '30px'
-    document.addEventListener('click', clickListener)
+    sidebarVisible = true
   }
 }
+
+function clickListener() {
+  if (sidebarVisible) {
+    switchNav()
+  }
+}
+
+document.addEventListener('click', clickListener, true)
